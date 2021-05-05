4444e98fe902c83c0f528f951cf6f020bb2f6215
Seb6277 <sebastien.carpentier89@gmail.com>
2021-05-05 11:37:06 +0200

    reformat PEP8

--- map_scrap.py
+++ map_scrap.py
@@ -20,9 +20,9 @@ categories = ['Cafe', 'Grocery Store','Bank','Liquor Store','Gas Station','Pharm
 # cursor = mydb.cursor()
 
 
-# def save_mysql(tablename, data):
-#     cursor.execute("CREATE TABLE IF NOT EXISTS" + " `"+tablename+"`(`name` TEXT,`address` TEXT,`latitude` TEXT,`longitude` TEXT)")
-#     insertstring = "INSERT INTO"+ " " + "`"+ tablename + "`(`name`,`address`,`latitude`,`longitude`) VALUES (%s,%s,%s,%s)"
+# def save_mysql(tablename, data): cursor.execute("CREATE TABLE IF NOT EXISTS" + " `"+tablename+"`(`name` TEXT,
+# `address` TEXT,`latitude` TEXT,`longitude` TEXT)") insertstring = "INSERT INTO"+ " " + "`"+ tablename + "`(`name`,
+# `address`,`latitude`,`longitude`) VALUES (%s,%s,%s,%s)"
     
 #     cursor.executemany(insertstring, data)
 #     mydb.commit()
@@ -52,8 +52,3 @@ with open("results.csv","w", newline='') as file:
     #         value.append((j['title'],j['address'],j['gps_coordinates']['latitude'],j['gps_coordinates']['longitude']))
     #     # save_mysql(i,value)
     #     writer.writerow([("ds","sg","ag","agh"),("ag","ah","aha","awe")])
-                       
-    
-    
-        
-        
